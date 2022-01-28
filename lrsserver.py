634ad57529ee5562b0fc734724ac5691a1d84f75
csgn <dev.csgn@gmail.com>
2022-01-28 23:37:32 +0300

    S

--- lrsserver.py
+++ lrsserver.py
@@ -1,34 +1,69 @@
-import os
-import eventlet
+import coloredlogs
+import logging
+import asyncio
 import socketio
 
-sio = socketio.Server()
-app = socketio.WSGIApp(sio)
+from aiohttp import web
 
 
-lrcs = []
+sio = socketio.AsyncServer(cors_allowed_origins=["*"])
+app = web.Application()
+sio.attach(app)
+
+coloredlogs.install()
+logging.basicConfig(level=logging.INFO)
 
 
 @sio.event
 def connect(sid, environ, auth):
-    lrcs.append(sid)
-    print("Connected ", sid, environ, auth)
+    logging.info(f"LRS_SERVER:CONNECT - SID[{sid}] - ALLOWED")
 
 
 @sio.event
-def login(sid, data):
-    print("LOGIN", sid, data)
+async def disconnect(sid):
+    logging.info(f"LRS_SERVER:DISCONNECT - SID[{sid}] - DISCONNECTED")
 
 
 @sio.event
-def disconnect(sid):
-    print("Disconnected ", sid)
+async def login(*args, **kwargs):
+    # TODO check sid in room
+    sid, data = args
+    response = {}
+    error = None
+
+    try:
+        sio.enter_room(sid, data["room"])
+        logging.info(f"LRS_SERVER:LOGIN - SID[{sid}] - ALLOWED")
+    except Exception as e:
+        logging.info(f"LRS_SERVER:LOGIN - SID[{sid}] - DENIED")
+        error = str(e)
+
+    if error:
+        response = {"error": error}
+
+    await sio.emit("login_resp", response, to=sid)
 
 
 @sio.event
-def forward(sid):
-    pass
+async def logout(*args, **kwargs):
+    # TODO check sid in room
+    sid, data = args
+    response = {}
+    error = None
+
+    try:
+        sio.leave_room(sid, data["room"])
+        logging.info(f"LRS_SERVER:LOGOUT - SID[{sid}] - ALLOWED")
+    except Exception as e:
+        logging.info(f"LRS_SERVER:LOGOUT - SID[{sid}] - DENIED")
+        error = str(e)
+
+    if error:
+        response = {"error": error}
+
+    await sio.emit("logout_resp", response, to=sid)
 
 
 if __name__ == "__main__":
-    eventlet.wsgi.server(eventlet.listen(('', int(os.environ["PORT"]))), app)
+    web.run_app(app)
+    #eventlet.wsgi.server(eventlet.listen(('', int(os.environ["PORT"]))), app)
