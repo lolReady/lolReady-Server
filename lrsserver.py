462743c76f7057a0f5d83018f44202f8aebbba06
csgn <dev.csgn@gmail.com>
2022-02-05 15:08:48 +0300

    Update

--- lrsserver.py
+++ lrsserver.py
@@ -1,5 +1,5 @@
-from cmath import log
 import os
+from pprint import pprint
 import coloredlogs
 import logging
 import asyncio
@@ -18,90 +18,79 @@ logging.basicConfig(level=logging.INFO)
 
 @sio.event
 def connect(sid, environ, auth):
-    logging.info(f"LRS_SERVER:CONNECT - SID[{sid}] - ALLOWED")
+    logging.info(f"LRS_SERVER<CONNECT> - {sid} - ALLOWED")
 
 
 @sio.event
 async def disconnect(sid):
-    logging.info(f"LRS_SERVER:DISCONNECT - SID[{sid}] - DISCONNECTED")
+    logging.info(f"LRS_SERVER<DISCONNECT> - {sid} - DISCONNECTED")
 
 
 @sio.event
-async def login(*args, **kwargs):
-    sid, data = args
-    response = {"data": data, "error": None}
+async def login(sid, payload):
+    response = {**payload, "error": None}
 
     try:
-        sio.enter_room(sid, data["room"])
+        sio.enter_room(sid, payload["room"])
         logging.info(
-            f"LRS_SERVER:LOGIN - [{sid}] entered to [{data['room']}]")
+            f"LRS_SERVER<LOGIN> - [{sid}] entered to [{payload['room']}]")
     except Exception as e:
         logging.info(
-            f"LRS_SERVER:LOGIN - [{sid}] not entered to [{data['room']}]")
+            f"LRS_SERVER:LOGIN - [{sid}] not entered to [{response['room']}]")
         response["error"] = str(e)
 
     await sio.emit("login_resp", response, to=sid)
 
 
 @sio.event
-async def logout(*args, **kwargs):
-    sid, data = args
-    response = {"data": data, "error": None}
+async def logout(sid, payload):
+    response = {**payload, "error": None}
 
     try:
-        sio.leave_room(sid, data["room"])
+        sio.leave_room(sid, payload["room"])
         logging.info(
-            f"LRS_SERVER:LOGOUT - SID[{sid}] from {data['room']} - ALLOWED")
+            f"LRS_SERVER<LOGOUT> - {sid} from {payload['room']} - ALLOWED")
     except Exception as e:
-        logging.info(f"LRS_SERVER:LOGOUT - SID[{sid}] - DENIED")
+        logging.info(f"LRS_SERVER<LOGOUT> - {sid} - DENIED")
         response["error"] = str(e)
 
     await sio.emit("logout_resp", response, to=sid)
 
 
 @sio.event
-async def ping(*args, **kwargs):
-    sid, data = args
-    logging.info(
-        f"LRS_SERVER:PING:{data['method']}:{data['endpoint']} - [{sid}] to [{data['room']}]")
-    await sio.emit("ping", data, room=data["room"], skip_sid=sid)
+async def ping(sid, payload):
+    logging.info(f"LRS_SERVER<PING> - {sid}")
+    await sio.emit("ping", payload, room=payload["room"], skip_sid=sid)
 
 
 @sio.event
-async def ping_resp(*args, **kwargs):
-    sid, data = args
-    response = {**data}
-    print(response)
-    await sio.emit("ping_resp", response, room=data["room"], skip_sid=sid)
+async def ping_resp(sid, payload):
+    logging.info(f"LRS_SERVER<PING_RESP> - {sid}")
+    await sio.emit("ping_resp", payload, room=payload["room"], skip_sid=sid)
 
 
 @sio.event
-async def subscribe(*args, **kwargs):
-    sid, data = args
-    logging.info(f"LRS_SERVER:SUBSCRIBE - SID[{sid}] - {data}")
-    print("YAYYYY", data.keys())
-    await sio.emit("subscribe", data, room=data["room"], skip_sid=sid)
+async def subscribe(sid, payload):
+    logging.info(f"LRS_SERVER<SUBSCRIBE> - {sid}")
+    await sio.emit("subscribe", payload, room=payload["room"], skip_sid=sid)
 
 
 @sio.event
-async def subscribe_resp(*args, **kwargs):
-    sid, data = args
-    print(data)
-    await sio.emit("subscribe_resp", data, room=data["room"], skip_sid=sid)
+async def subscribe_resp(sid, payload):
+    logging.info(f"LRS_SERVER<SUBSCRIBE_RESP> - {sid}")
+    await sio.emit("subscribe_resp", payload, room=payload["room"], skip_sid=sid)
 
 
 @sio.event
-async def unsubscribe(*args, **kwargs):
-    sid, data = args
-    logging.info(f"LRS_SERVER:UNSUBSCRIBE - SID[{sid}] - {data}")
-    print("YAYYYY", data.keys())
-    await sio.emit("unsubscribe", data, room=data["room"], skip_sid=sid)
+async def unsubscribe(sid, payload):
+    logging.info(f"LRS_SERVER<UNSUBSCRIBE> - {sid}")
+    await sio.emit("unsubscribe", payload, room=payload["room"], skip_sid=sid)
 
 
 @sio.event
-async def unsubscribe_resp(*args, **kwargs):
-    sid, data = args
-    await sio.emit("unsubscribe_resp", data, room=data["room"], skip_sid=sid)
+async def unsubscribe_resp(sid, payload):
+    logging.info(f"LRS_SERVER<UNSUBSCRIBE_RESP> - {sid}")
+    await sio.emit("unsubscribe_resp", payload, room=payload["room"], skip_sid=sid)
 
 
 if __name__ == "__main__":
